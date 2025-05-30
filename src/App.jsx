dd2f59de55d3bb89a266d9bce79a6800541f8f81
s1dhant <sidhantiyer4@gmail.com>
2025-05-30 17:29:36 +1000

    react

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Hero from './components/Hero';
+
+function App() {
+  return (
+    <>
+      <Hero />
+      {/* Other sections like About, Projects, etc. */}
+    </>
+  );
+}
+
+export default App;
